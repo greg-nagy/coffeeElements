cf8fe0d8da9528455f1e0674a97d60ef757abcf7
Nagy Gergo <nagy.gergo@3pixel.hu>
2018-06-28 00:01:32 +0200

    look ma' I am a custom element!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoffeeComponent } from './coffee.component';
 
 import { createCustomElement } from '@angular/elements';
 
 @NgModule({
-  declarations: [AppComponent, CoffeeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [CoffeeComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent],
   entryComponents: [CoffeeComponent]
 })
 export class AppModule {
